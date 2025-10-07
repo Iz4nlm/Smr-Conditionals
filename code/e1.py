96fc09034fdab41ae5d263bdccbe3fc73464fd3c
Iz4nlm <4064343@alu.murciaeduca.es>
2025-10-07 20:44:25 +0200

    e1 actualizado subido

--- code/e1.py
+++ code/e1.py
@@ -8,7 +8,15 @@ menor: int
 if (x >=20 and x <30) and (y >=20 and x <30):
     if x > y:
         mayor = x
+        menor = y
     else:
         mayor = y
+        menor = x
+    print(mayor)
 
-    print(mayor,menor)
+
+# esto no hace falta, esta mejor la de arriba, es mas productivo
+if (x < y):
+    x = menor
+else: 
+    y = menor
