606e945a509a60b94ea5b7d026c4f52d41016de0
Iz4nlm <4064343@alu.murciaeduca.es>
2025-10-07 20:28:52 +0200

    Ejercicio a1 terminado

---
+++ e1.py
@@ -0,0 +1,14 @@
+# Creacion de variables
+
+x: int = int(input("introduce X: "))
+y: int = int(input("introduce Y: "))
+mayor: int 
+menor: int
+
+if (x >=20 and x <30) and (y >=20 and x <30):
+    if x > y:
+        mayor = x
+    else:
+        mayor = y
+
+    print(mayor,menor)
